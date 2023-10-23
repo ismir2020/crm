1adbfaafa1476c32e03dfbd21fa33e5f83f3fb42
ismir2020 <76167352+ismir2020@users.noreply.github.com>
2023-10-23 12:58:31 +0000

    Oct 23, 2023, 2:58 PM

--- resources/views/leads.blade.php
+++ resources/views/leads.blade.php
@@ -1,34 +1,56 @@
 <!-- resources/views/leads.blade.php --> 
- 
+
 @include('layouts.header')    
- 
+
 <h1>Leads für Benutzer {{$user->name}}</h1> 
- 
-<table id="leads" class="table table-striped table-bordered" style="width:100%"> 
-    <thead> 
-        <tr> 
-            <th>Name</th> 
-            <th>E-Mail</th> 
-        </tr> 
-    </thead> 
-    <tbody> 
-        @foreach ($leads as $lead) 
-            <tr> 
-                <td>{{$lead->name}}</td> 
-                <td>{{$lead->email}}</td> 
-            </tr> 
-        @endforeach 
-    </tbody> 
-</table> 
- 
-<script> 
-$(document).ready( function () { 
-    $('#leads').DataTable(); 
-} ); 
-</script> 
- 
-<!-- DataTables CSS & JS via CDN --> 
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css"> 
-<script src="https://code.jquery.com/jquery-3.5.1.js">> 
-<script  src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js">> 
-<script  src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js">>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <form class="form-inline float-left">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    <div class="float-right">
+                        <select class="custom-select" style="width: auto;">
+                            <option selected>10</option>
+                            <option value="1">25</option>
+                            <option value="2">50</option>
+                            <option value="3">100</option>
+                        </select>
+                        <span class="text-secondary mx-2">Einträge</span>
+                        <span class="text-secondary">von 200</span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table id="leads" class="table table-striped table-bordered nowrap" style="width:100%"> 
+                        <thead> 
+                            <tr> 
+                                <th>Name</th> 
+                                <th>E-Mail</th> 
+                            </tr> 
+                        </thead> 
+                        <tbody> 
+                            @foreach ($leads as $lead) 
+                                <tr> 
+                                    <td>{{$lead->name}}</td> 
+                                    <td>{{$lead->email}}</td> 
+                                </tr> 
+                            @endforeach 
+                        </tbody> 
+                    </table> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+$(document).ready( function () {
+    $('#leads').DataTable({
+        responsive: true
+    });
+} );
+</script>
